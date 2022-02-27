c8e086177dbf0588b9473396b6bb50ea742778f2
John Watson <johnwatson484@hotmail.com>
2022-02-27 15:56:15 +0000

    Add confirmation

--- create-acknowledgement/index.js
+++ create-acknowledgement/index.js
@@ -1,3 +1,9 @@
 module.exports = async function (context, myBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
-};
+  context.log('JavaScript blob trigger function processed blob \n Blob:', context.bindingData.blobTrigger, '\n Blob Size:', myBlob.length, 'Bytes')
+  context.bindings.myOutputBlob = context.bindings.myBlob
+  context.bindings.outputSbTopic = {
+    fileReceived: context.bindingData.blobTrigger,
+    acknowledgementFile: `${context.bindingData.blobTrigger}.ack`,
+    acknowledged: new Date()
+  }
+}
